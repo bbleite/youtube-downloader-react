95b9ccf83530d8a62bde115a32a547da0d047052
Bernardo Leite <bbleite85@gmail.com>
2023-08-31 22:32:41 -0500

    handleSubmit, youtube api setup, axios call

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useRef, useState } from "react"
+import { youtube_parser } from "./utils";
 
 function App() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputUrlRef);
+    console.log(inputUrlRef.current.value);
+    const youtubeID = youtube_parser(inputUrlRef.current.value);
+    console.log(youtubeID);
+
+    const options = {
+      method: 'GET',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      params: {
+        id: youtubeID
+      },
+      headers: {
+        'X-RapidAPI-Key': '0a3d849cedmshc6ee1c457cfc9adp1eefc3jsn74d2c60a2643',
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+      }
+    };
+    
+    axios(options)
+    .then(res => setUrlResult(res.data),
+      console.log(res),
+      console.log(res.data))
+    .catch(err => console.log(err))
+  }
 
   return (
     <div className="app">
@@ -10,14 +40,12 @@ function App() {
         Transform YouTube videos into MP3s in just a few clicks!
       </p>
 
-      <form className="form">
-        <input placeholder="Paste a YouTube video URL link..." className="form_input" type="text" />
+      <form onSubmit={handleSubmit} className="form">
+        <input ref={inputUrlRef} placeholder="Paste a YouTube video URL link..." className="form_input" type="text" />
         <button type="submit" className="form_button">Search</button>
       </form>
 
       <a href="" className="download_btn">Download MP3</a>
-
-
     </section>
     </div>
   )
